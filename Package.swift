0b710a3b4253421f0f5440c4b9f5b06664882f2d
anhnt2 <anhnt2@nal.vn>
2022-07-21 15:08:33 +0700

    downgrate to ios11

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EkycVideoCallModule",
-    platforms: [.iOS(.v13), .macOS(.v11)],
+    platforms: [.iOS(.v11), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,20 +14,18 @@ let package = Package(
             targets: ["EkycVideoCallModule"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/cbajapan/fcsdk-ios.git", from: "4.0.0")
+        .package(url: "https://github.com/DexJoke/swift-fcsdk-ios.git", from: "4.0.2")
     ],
     targets: [
         .target(
             name: "EkycVideoCallModule",
             dependencies: [
-                .product(name: "FCSDKiOS", package: "fcsdk-ios")
+                "FCSDKiOS"
             ],
             path: "Sources"
         ),
         .testTarget(
             name: "EkycVideoCallModuleTests",
             dependencies: ["EkycVideoCallModule"]),
-//        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.0.1.xcframework.zip", checksum: "671482c898f37e66afd20e0a110cbb5dbec5f6e358f8ae9cacde2e56fa2a3ee1"),
-//        .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m95-1.0.1.xcframework.zip", checksum: "b40b7d5b08dbe11f18d60779ffb0cd6576e8e45069d22d81b016a23db88a9633")
     ]
 )
