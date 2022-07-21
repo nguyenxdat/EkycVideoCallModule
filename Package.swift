c4b9138bc1978ec140d6c7903d5ca13b1e562c94
anhnt2 <anhnt2@nal.vn>
2022-07-21 09:28:03 +0700

    update

--- Package.swift
+++ Package.swift
@@ -19,11 +19,15 @@ let package = Package(
     targets: [
         .target(
             name: "EkycVideoCallModule",
-            dependencies: []),
+            dependencies: [
+                .product(name: "FCSDKiOS", package: "fcsdk-ios")
+            ],
+            path: "Sources"
+        ),
         .testTarget(
             name: "EkycVideoCallModuleTests",
             dependencies: ["EkycVideoCallModule"]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.0.1.xcframework.zip", checksum: "671482c898f37e66afd20e0a110cbb5dbec5f6e358f8ae9cacde2e56fa2a3ee1"),
-        .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m95-1.0.1.xcframework.zip", checksum: "b40b7d5b08dbe11f18d60779ffb0cd6576e8e45069d22d81b016a23db88a9633")
+//        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.0.1.xcframework.zip", checksum: "671482c898f37e66afd20e0a110cbb5dbec5f6e358f8ae9cacde2e56fa2a3ee1"),
+//        .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m95-1.0.1.xcframework.zip", checksum: "b40b7d5b08dbe11f18d60779ffb0cd6576e8e45069d22d81b016a23db88a9633")
     ]
 )
--- Sources/EkycVideoCallModule/EkycVideoCallModule.swift
+++ Sources/EkycVideoCallModule/EkycVideoCallModule.swift
@@ -2,6 +2,7 @@ import FCSDKiOS
 
 public struct EkycVideoCallModule {
     public private(set) var text = "Hello, World!"
+    private var acbuc: ACBUC!
 
     public init() {
     }
